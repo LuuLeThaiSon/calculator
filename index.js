75142deab9602f7c539ff8d37918430fc86c5acc
LuuLeThaiSon <llts28497@gmail.com>
2022-09-08 21:53:57 +0700

    done

--- index.js
+++ index.js
@@ -24,14 +24,43 @@ function rad() {
 }
 
 function factorial() {
-     debugger
+     
      let result = 1;
 
-     for (let i=1 ; i < resultBox.value; i++) {
-          result = result * i;
-          return;
+     for (let i=1; i<=resultBox.value;i++) {
+          result *= i;
      }
-
+     
      resultBox.value = result;
 
+}
+
+function percent() {
+     let result = resultBox.value / 100;
+
+     resultBox.value = result;
+}
+
+function tan() {
+     let result = Math.tan(resultBox.value);
+
+     resultBox.value = result;
+}
+
+function sin() {
+     let result = Math.sin(resultBox.value);
+
+     resultBox.value = result;
+}
+
+function log() {
+     let result = Math.log(resultBox.value);
+
+     resultBox.value = result;
+}
+
+function cos() {
+     let result = Math.cos(resultBox.value);
+
+     resultBox.value = result;
 }
